7afcd0788ad46d3d215ccebd9bacabc7fdfec869
Fantasy <805841483@qq.com>
2018-01-21 22:03:31 +0800

    feat:添加数据模型类
    添加数据模型类

---
+++ eleven-model/src/main/java/com/fantasy/eleven/model/PermissionDO.java
@@ -0,0 +1,56 @@
+package com.fantasy.eleven.model;
+
+import java.util.Date;
+
+/**
+ * Created by Fantasy on 2018/1/21.
+ *
+ * @author Fantasy
+ */
+public class PermissionDO {
+    private Integer id;
+    private String permissionName;
+    private Integer roleId;
+    private Date gmtCreate;
+    private Date gmtModified;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPermissionName() {
+        return permissionName;
+    }
+
+    public void setPermissionName(String permissionName) {
+        this.permissionName = permissionName;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
---
+++ eleven-model/src/main/java/com/fantasy/eleven/model/RoleDO.java
@@ -0,0 +1,47 @@
+package com.fantasy.eleven.model;
+
+import java.util.Date;
+
+/**
+ * Created by Fantasy on 2018/1/21.
+ *
+ * @author Fantasy
+ */
+public class RoleDO {
+    private Integer id;
+    private String roleName;
+    private Date gmtCreate;
+    private Date gmtModified;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Date getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Date gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
