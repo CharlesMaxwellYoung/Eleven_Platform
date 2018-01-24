1197e4873b46d6869a36b56d85aaf0b09f8563f7
Fantasy <805841483@qq.com>
2018-01-24 23:50:54 +0800

    feat:添加登录页面
    添加404页面

--- eleven-web/src/main/java/com/fantasy/eleven/controller/UserController.java
+++ eleven-web/src/main/java/com/fantasy/eleven/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.fantasy.eleven.controller;
 
 import com.fantasy.eleven.model.UserDO;
+import com.fantasy.eleven.realm.MyRealm;
 import com.fantasy.eleven.service.UserService;
+import org.apache.log4j.Logger;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +23,8 @@ import java.util.List;
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    private static Logger log = Logger.getLogger(UserController.class);
     @Resource
     private UserService userService;
 
@@ -54,5 +61,22 @@ public class UserController {
     @RequestMapping("/login")
     public String userLogin() {
         return "Login";
+    }
+
+    @RequestMapping("/userLogin")
+    public void userSignInByNameAndPassword(UserDO userDO) {
+        Subject userSubject = SecurityUtils.getSubject();
+        UsernamePasswordToken uToken = new UsernamePasswordToken(userDO.getUserName(), userDO.getUserPassword());
+        try {
+            userSubject.login(uToken);
+        } catch (UnknownAccountException e) {
+            log.debug("用户名不存在");
+        } catch (IncorrectCredentialsException e) {
+            log.debug("用户密码不正确");
+        } catch (LockedAccountException e) {
+            log.debug("用户被锁");
+        } catch (AuthenticationException e) {
+            log.debug("其他错误");
+        }
     }
 }
