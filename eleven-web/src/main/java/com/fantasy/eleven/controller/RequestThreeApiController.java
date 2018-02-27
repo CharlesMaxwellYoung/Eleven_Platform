dc88548e51124478276d95fff8a2984732456d6c
Fantasy <805841483@qq.com>
2018-02-27 22:54:39 +0800

    :sparkles: 添加公共配置文件
    添加配置信息
    添加调用第三方接口

---
+++ eleven-web/src/main/java/com/fantasy/eleven/controller/RequestThreeApiController.java
@@ -0,0 +1,15 @@
+package com.fantasy.eleven.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Fantasy on 2018/2/27.
+ *
+ * @author Fantasy
+ */
+@Controller
+@RequestMapping("ThreeApi")
+public class RequestThreeApiController {
+
+}
